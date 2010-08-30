92fc4c5f0d16cd9c37f2a66b8ba3a50771452ff5
kfl62 <kfl62g@gmail.com>
2010-08-30 11:57:29 +0300

    Initiate Compass css framework

---
+++ config/compass.rb
@@ -0,0 +1,18 @@
+if defined?(TrstSys)
+  # This is the configuration to use when running within sinatra
+  project_path = TrstSys.root
+  environment = :development
+else
+  # this is the configuration to use when running within the compass command line tool.
+  css_dir = File.join('public', 'stylesheets')
+  relative_assets = true
+  environment = :production
+end
+
+# This is common configuration
+sass_dir = File.join('src', 'stylesheets')
+images_dir = File.join('public', 'images')
+http_path = '/'
+http_images_path = '/images'
+http_stylesheets_path = '/stylesheets'
+sass_options = {:cache_location => './tmp/sass-cache'}
--- lib/trst_sys.rb
+++ lib/trst_sys.rb
@@ -1,8 +1,28 @@
 require 'bundler/setup'
 require 'sinatra/base'
+require 'compass'
 
 class TrstSys < Sinatra::Base
+
+  configure do
+    root = File.expand_path('..', File.dirname(__FILE__))
+    set :root , root
+    set :views, File.join(root, 'src')
+    set :logging, true
+    Compass.add_project_configuration(File.join(root, 'config', 'compass.rb'))
+  end
+
+  get '/stylesheets/:name.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    sass :"stylesheets/#{params[:name]}", Compass.sass_engine_options
+  end
+
+  get '/help' do
+    "Some help"
+  end
+
   get '/' do
     "Welcome to TrustSys application ...testing..."
   end
+
 end
