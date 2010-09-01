e268cbee8266739db901e492da4b17aa24e5a27a
kfl62 <kfl62g@gmail.com>
2010-09-01 17:20:23 +0300

    Simpler way to retrieve layout

--- lib/trst_pub.rb
+++ lib/trst_pub.rb
@@ -2,26 +2,19 @@ require 'trst_conf'
 
 class TrstPub < Sinatra::Base
   
-  Dir[File.join(TrstPub.views,"layouts","*.haml")].each do |layout|
-    name = layout[/([^\/]*)\.haml$/, 1].to_sym
-    haml = File.read(layout)
-    TrstPub.template(name) {haml}
-    TrstPub.layout {haml} if name == :trst_pub
-  end
-
   get '/stylesheets/:name.css' do
     content_type 'text/css', :charset => 'utf-8'
     sass :"stylesheets/#{params[:name]}", Compass.sass_engine_options
   end
 
   get '/' do
-    haml :"/trst_pub/index", :layout => !request.xhr?
+    haml :"/trst_pub/index", :layout => request.xhr? ? false : :'layouts/trst_pub'
   end
 
   get '/:page' do |pg|
     page = pg.split('.')
     page.empty? ? page = 'index' : page = page[0]
-    haml :"/trst_pub/#{page}", :layout => !request.xhr?
+    haml :"/trst_pub/#{page}", :layout => request.xhr? ? false : :'layouts/trst_pub'
   end
 
 end
