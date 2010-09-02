89e83945b775b1947648e076cbcfa1c14b739af3
kfl62 <kfl62g@gmail.com>
2010-09-02 11:22:05 +0300

    Models: empty, just for testing

---
+++ lib/models/pg_pub.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+class PgPub
+  include Mongoid::Document
+  include Mongoid::Timestamps
+end
---
+++ lib/models/pg_sys.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+class PgSys
+  include Mongoid::Document
+  include Mongoid::Timestamps
+end
