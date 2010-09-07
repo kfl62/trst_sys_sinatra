6572f579118212eab6cfafae67f6d458c061542f
kfl62 <kfl62g@gmail.com>
2010-09-07 08:55:20 +0300

    Render menu for trst_sys

--- lib/trst_sys.rb
+++ lib/trst_sys.rb
@@ -6,16 +6,30 @@ class TrstSys < Sinatra::Base
     sass :"stylesheets/#{params[:name]}", Compass.sass_engine_options
   end
 
-  get '/' do
-    login_required
-    haml :"/trst_sys/index", :layout => request.xhr? ? false : :'layouts/trst_sys'
-  end
-
-  get '/:page' do |pg|
-    login_required
-    page = pg.split('.')
-    page.empty? ? page = 'index' : page = page[0]
-    haml :"/trst_sys/#{page}", :layout => request.xhr? ? false : :'layouts/trst_sys'
+  get '/*' do
+    if request.xhr?
+      method, id = params[:splat][0].split('_')
+      page = TrstBook.send method, id
+      haml :"trst_sys/page", :layout  => false, :locals => {:page  => page}
+    else
+      # code below just for testing :) real route above
+      # TODO not working on localized real pathts
+      book = TrstBook.where(:name  => "trst_sys").first
+      if params[:splat][0] == ""
+        page = book.chapters.where(:slug  => "my_page").first
+      elsif File.basename(params[:splat][0]) == "index.html"
+        if File.dirname(params[:splat][0]) == "."
+          page = book.chapters.where(:slug  => "my_page").first
+        else
+          page = book.chapters.where(:slug  => File.dirname(params[:splat][0])).first
+        end
+      else
+        chapter = book.chapters.where(:slug  => File.dirname(params[:splat][0])).first
+        slug = File.basename(params[:splat][0]).gsub(/(TrustSys-#{chapter.slug.camelize}-)|(.html)/,"")
+        page = chapter.pages.where(:slug => slug).first
+      end
+      haml :"trst_sys/page", :layout => :'/layouts/trst_sys', :locals => {:page  => page}
+    end
   end
 
 end
