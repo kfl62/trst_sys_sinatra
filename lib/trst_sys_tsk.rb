ddd8e882013a1e4935caa45ba4588afb3f35788d
kfl62 <kfl62g@gmail.com>
2010-09-11 08:08:40 +0300

    task: main route

--- lib/trst_sys_tsk.rb
+++ lib/trst_sys_tsk.rb
@@ -6,4 +6,20 @@ class TrstSysTsk < Sinatra::Base
     haml :'/trst_sys/daily_tasks', :locals => {:tsks  => session[:tasks]}
   end
 
+  get '/:verb/:id' do |verb,id|
+    task = TrstTask.find(id)
+    model, method = task.target.split('.')
+    haml_path = task.haml_path
+    @task = task
+    @object = model.constantize.send method, id
+    case verb
+    when /get|delete/
+      haml :"#{haml_path}/get_delete", :layout => false, :locals => {:action => verb}
+    when /post|put/
+      haml :"#{haml_path}/post_put", :layout => false, :locals => {:action => verb}
+    else
+      haml "%p Wrong verb #{params.inspect}"
+    end
+  end
+
 end
