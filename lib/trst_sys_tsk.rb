16fb9277b984a15ac0c6b70eb70ed8ad06526435
kfl62 <kfl62g@gmail.com>
2010-09-14 19:20:13 +0300

    separate help route and add filter(select)

--- lib/trst_sys_tsk.rb
+++ lib/trst_sys_tsk.rb
@@ -6,19 +6,39 @@ class TrstSysTsk < Sinatra::Base
     haml :'/trst_sys/daily_tasks', :locals => {:tsks  => session[:tasks]}
   end
 
-  get '/:verb/:id' do |verb,id|
+  get '/:id/help' do |id|
+    @task = TrstTask.find(id)
+    haml ":markdown\n  #{@task.help}"
+  end
+
+  get '/:id/filter' do |id|
     task = TrstTask.find(id)
     model, method = task.target.split('.')
     haml_path = task.haml_path == 'default' ? '/trst_sys/shared' : task.haml_path
     @task = task
-    @object = model.constantize.send method, id
+    @object = model.constantize.all
+    if @object.empty?
+      haml :"#{haml_path}/post_put", :layout => false, :locals => {:action => 'post'}
+    else
+      haml :"#{haml_path}/filter", :layout => false, :locals => {:action => 'filter'}
+    end
+  end
+
+  get '/:id/:verb/:target_id' do |id,verb,target_id|
+    task = TrstTask.find(id)
+    model, method = task.target.split('.')
+    haml_path = task.haml_path == 'default' ? '/trst_sys/shared' : task.haml_path
+    @task = task
+    if target_id == 'new'
+      @object =  model.constantize 
+    else 
+      @object = model.constantize.send method, target_id
+    end
     case verb
     when /get|delete/
       haml :"#{haml_path}/get_delete", :layout => false, :locals => {:action => verb}
     when /post|put/
       haml :"#{haml_path}/post_put", :layout => false, :locals => {:action => verb}
-    when /help/
-      haml "#{@task.help}"
     else
       haml "%p Wrong verb #{params.inspect}"
     end
