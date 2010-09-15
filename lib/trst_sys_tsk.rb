20b911d5a87819d275801ecbd69427c6041f2de4
kfl62 <kfl62g@gmail.com>
2010-09-15 11:56:57 +0300

    REST: first version

--- lib/trst_sys_tsk.rb
+++ lib/trst_sys_tsk.rb
@@ -5,12 +5,12 @@ class TrstSysTsk < Sinatra::Base
     session[:daily_tasks] = true
     haml :'/trst_sys/daily_tasks', :locals => {:tsks  => session[:tasks]}
   end
-
+  # route for help {{{1
   get '/:id/help' do |id|
     @task = TrstTask.find(id)
     haml ":markdown\n  #{@task.help}"
   end
-
+  # route for filter {{{1
   get '/:id/filter' do |id|
     task = TrstTask.find(id)
     model, method = task.target.split('.')
@@ -18,12 +18,13 @@ class TrstSysTsk < Sinatra::Base
     @task = task
     @object = model.constantize.all
     if @object.empty?
+      @object = model.constantize
       haml :"#{haml_path}/post_put", :layout => false, :locals => {:action => 'post'}
     else
       haml :"#{haml_path}/filter", :layout => false, :locals => {:action => 'filter'}
     end
   end
-
+  # route for get (control center) {{{1
   get '/:id/:verb/:target_id' do |id,verb,target_id|
     task = TrstTask.find(id)
     model, method = task.target.split('.')
@@ -42,6 +43,51 @@ class TrstSysTsk < Sinatra::Base
     else
       haml "%p Wrong verb #{params.inspect}"
     end
+  end
+  # route for post(create) {{{1
+  post '/:id/:verb/:target_id' do |id,verb,target_id|
+    task = TrstTask.find(id)
+    model, method = task.target.split('.')
+    haml_path = task.haml_path == 'default' ? '/trst_sys/shared' : task.haml_path
+    case method
+    when 'find'
+      @task = task
+      @object = model.constantize.create
+    else
+      params.inspect
+    end
+    flash[:msg] = {:msg => {:txt => I18n.t('db.post', :data => model), :class => "info"}}.to_json
+    haml :"#{haml_path}/post_put", :layout => false, :locals => {:action => 'put'}
+  end
+  # route for put (update) {{{1
+  put '/:id/:verb/:target_id' do |id,verb,target_id|
+    task = TrstTask.find(id)
+    model, method = task.target.split('.')
+    haml_path = task.haml_path == 'default' ? '/trst_sys/shared' : task.haml_path
+    case method
+    when 'find'
+      @task = task
+      @object = model.constantize.send method, target_id
+      @object.update_attributes params[model.underscore.to_sym]
+    else
+      params.inspect
+    end
+    flash[:msg] = {:msg => {:txt => I18n.t('db.put', :data => model), :class => "info"}}.to_json
+    haml :"#{haml_path}/get_delete", :layout => false, :locals => {:action => 'get'}
+  end
+  # route for delete {{{1
+  delete '/:id/:verb/:target_id' do |id,verb,target_id|
+    task = TrstTask.find(id)
+    model, method = task.target.split('.')
+    case method
+    when 'find'
+      @task = task
+      @object = model.constantize.send method, target_id
+      @object.delete
+    else
+      params.inspect
+    end
+    flash[:msg] = {:msg => {:txt => I18n.t('db.delete', :data => @object.name), :class => "info"}}.to_json
   end
 
 end
--- public/javascripts/trust_sys/task.js
+++ public/javascripts/trust_sys/task.js
@@ -1,5 +1,6 @@
 dojo.provide("trst.task");
 var task = {
+  // variables {{{1
   id: "",
   verb: "",
   target_id: "",
@@ -8,6 +9,8 @@ var task = {
   actions: new Array,
   taskOverlay: dojo.create('div',{id:"task_overlay"}),
   taskWindow: dojo.create('div',{id:"task_window"}),
+  // functions {{{1
+  // init {{{2
   init: function(id,verb,target_id){
     this.id = id;
     if (verb == undefined){
@@ -18,7 +21,7 @@ var task = {
       this.verb = "help"
       this.url.push(this.id,this.verb);
       this[this.verb]()
-    }else{ 
+    }else{
       var referrer = /filter|get/
       if (referrer.test(this.verb)){
         this.url.push(this.id,verb,target_id);
@@ -31,6 +34,7 @@ var task = {
       }
     }
   },
+  // filter {{{2
   filter: function(){
     var xhrArgs = {
       url: this.url.join('/'),
@@ -39,13 +43,14 @@ var task = {
         dojo.attr('xhr_msg','class','hidden');
       },
       error: function(error){
-        dojo.publish('xhrMsg',['error','error',error])
+        dojo.publish('xhrMsg',['error','error',error]);
       }
     };
     dojo.publish('xhrMsg',['loading','info']);
     var deferred = dojo.xhrGet(xhrArgs);
     this.url = ["/srv/tsk"];
   },
+  // help {{{2
   help: function(){
     var xhrArgs = {
       url: this.url.join('/'),
@@ -54,13 +59,14 @@ var task = {
         dojo.attr('xhr_msg','class','hidden');
       },
       error: function(error){
-        dojo.publish('xhrMsg',['error','error',error])
+        dojo.publish('xhrMsg',['error','error',error]);
       }
     };
     dojo.publish('xhrMsg',['loading','info']);
     var deferred = dojo.xhrGet(xhrArgs);
     this.url = ["/srv/tsk"];
   },
+  // get {{{2
   get: function(){
     xhrArgs = {
       url: this.url.join('/'),
@@ -69,13 +75,59 @@ var task = {
         dojo.attr('xhr_msg','class','hidden');
       },
       error: function(error){
-      dojo.publish('xhrMsg',['error','error',error])
+        dojo.publish('xhrMsg',['error','error',error]);
       }
     };
     dojo.publish('xhrMsg',['loading','info']);
     var deferred = dojo.xhrGet(xhrArgs);
     this.url = ["/srv/tsk"];
   },
+  // post {{{2
+  post: function(){
+    xhrArgs = {
+      url: this.url.join('/'),
+      load: function(data){
+        dojo.publish('xhrMsg',['flash']);
+        task.drawBox(data);
+      },
+      error: function(error){
+        dojo.publish('xhrMsg',['error','error',error]);
+      }
+    };
+    var deferred = dojo.xhrPost(xhrArgs);
+    this.url = ["/srv/tsk"];
+  },
+  // put {{{2
+  put: function(){
+    xhrArgs = {
+      form: dojo.query('form')[0],
+      url: this.url.join('/'),
+      load: function(data){
+        dojo.publish('xhrMsg',['flash']);
+        task.drawBox(data);
+      },
+      error: function(error){
+        dojo.publish('xhrMsg',['error','error',error]);
+      }
+    };
+    var deferred = dojo.xhrPut(xhrArgs);
+    this.url = ["/srv/tsk"];
+  },
+  // delete {{{2
+  delete: function(){
+    xhrArgs = {
+      url: this.url.join('/'),
+      load: function(data){
+        dojo.publish('xhrMsg',['flash']);
+      },
+      error: function(error){
+        dojo.publish('xhrMsg',['error','error',error]);
+      }
+    };
+    var deferred = dojo.xhrDelete(xhrArgs);
+    this.destroy();
+  },
+  // position the taskWindow{{{2
   positionBox: function(o1,o2){
     var lastX,lastY,tskww,tskwh,
     base = dojo.position(o1),
@@ -87,6 +139,7 @@ var task = {
       top:    lastY + 'px'
     });
   },
+  // draw the taskWindow{{{2
   drawBox: function(data){
     if (dojo.query('[id^="task"]').length == 0){
       var ovl = this.taskOverlay;
@@ -104,11 +157,13 @@ var task = {
       dojo.place(win,ovl,'after');
     }
   },
+  // destroy the taskWindow and reset vars{{{2
   destroy: function(){
     dojo.query('[id^="task"]').forEach("dojo.destroy(item)");
     this.id = this.verb = this.target_id = "";
     this.url = ["/srv/tsk"];
   },
+  // connect tasks in sidebar{{{2
   connect: function(){
     dojo.forEach(this.connections, dojo.disconnect);
     this.connections.length = 0;
@@ -122,6 +177,7 @@ var task = {
     })
   }
 }
+  // initialize on load{{{2
 function init(){
   task.connect();
 }
