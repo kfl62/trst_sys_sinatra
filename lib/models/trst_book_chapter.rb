e004b147184818822e7b1d04955ee94d269267b9
kfl62 <kfl62g@gmail.com>
2010-09-22 18:59:54 +0300

    TrstBookChapter#table_data and translations

--- lib/models/trst_book_chapter.rb
+++ lib/models/trst_book_chapter.rb
@@ -10,11 +10,20 @@ class TrstBookChapter
   localized_field :title
   localized_field :content
 
+  embedded_in :book, :inverse_of => :chapters
+  embeds_many :pages, :class_name => "TrstBookPage"
+
   def path
     slug == "home" ? retval = "/index.html" : retval = "/#{slug}/index.html"
     retval
   end
 
-  embedded_in :book, :inverse_of => :chapters
-  embeds_many :pages, :class_name => "TrstBookPage"
+  def table_data
+    [{:css => "integer",:name => "order",:label => I18n.t("trst_book_chapter.order"),:value => order},
+     {:css => "normal",:name => "slug",:label => I18n.t("trst_book_chapter.slug"),:value => slug},
+     {:css => "translated",:name => "name",:label => I18n.t("trst_book_chapter.name"),:value => name},
+     {:css => "translated",:title => "title",:label => I18n.t("trst_book_chapter.title"),:value => title},
+     {:css => "translated",:content => "content",:label => I18n.t("trst_book_chapter.content"),:value => content}]
+  end
+
 end
