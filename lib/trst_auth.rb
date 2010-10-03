0961a097f13ce03fb98fa7945aabaf0b0b08af95
kfl62 <kfl62g@gmail.com>
2010-10-03 20:16:45 +0300

    Handling authentication
    Just for login and logout.
    Only `admin` has rights to `add, delete, modify` users. In conclusion users are
    handled by a `task` (which requires {TrstUser#admin?} ) with all `CRUD` operations.
    Each user can change his password and some settings( {TrstUser} ) using an other `task`
    (accessible via `Settings` menu)

--- lib/trst_auth.rb
+++ lib/trst_auth.rb
@@ -1,110 +1,44 @@
 # encoding: utf-8
 =begin
-#Handling users#
+#Handling authentication#
+Just for login and logout.<br>
+Only `admin` has rights to `add, delete, modify` users. In conclusion users are
+handled by a `task` (which requires {TrstUser#admin?} ) with all `CRUD` operations.<br>
+Each user can change his password and some settings( {TrstUser} ) using an other `task`
+(accessible via `Settings` menu).
 =end
 class TrstAuth < Sinatra::Base
+
   # @todo Document this method
   get '/stylesheets/:name.css' do
     content_type 'text/css', :charset => 'utf-8'
     sass :"stylesheets/#{params[:name]}", Compass.sass_engine_options
   end
-  # @todo Document this method
-  get '/users' do
-    login_required
-    redirect "/" unless current_user.admin?
-    @users = TrstUser.all
-    if @users != []
-      haml :"/trst_auth/index", :layout => false
-    else
-      redirect '/adduser'
-    end
-  end
-  # @todo Document this method
-  get '/users/:id' do
-    login_required
-    @user = TrstUser.find(params[:id])
-    haml :"/trst_auth/show", :layout => false
-  end
+
   # @todo Document this method
   get '/login' do
     haml :"/trst_auth/login", :layout => request.xhr? ? false : :'layouts/trst_pub'
   end
+
   # @todo Document this method
   post '/login' do
     if user = TrstUser.authenticate(params[:login_name], params[:password])
       session[:user] = user.id
       session[:tasks] = user.daily_tasks
       flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.login_msg'), :class => "info"}}.to_json
-    if session[:return_to]
-        redirect_url = session[:return_to]
-        session[:return_to] = false
-        redirect redirect_url
-      else
-        redirect "#{lang_path}/srv"
-      end
+      redirect "#{lang_path}/srv"
     else
       flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.login_err'), :class => "error"}}.to_json
       redirect "#{lang_path}/"
     end
   end
+
   # @todo Document this method
   get '/logout' do
     session[:user] = nil
     session[:daily_tasks] = nil
     flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.logout_msg'), :class => "info"}}.to_json
     redirect "#{lang_path}/"
-  end
-  # @todo Document this method
-  get '/adduser' do
-    haml :"/auth/adduser", :layout => false
-  end
-  # @todo Document this method
-  post '/adduser' do
-    @user = TrstUser.set(params[:user])
-    if @user.valid && @user.id
-      session[:user] = @user.id
-      flash[:msg] =  {:msg => {:txt => I18n.t('trst_auth.adduser_msg'), :class => "info"}}.to_json
-      redirect "#{lang_path}/srv"
-    else
-      flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.adduser_msg') + @user.errors, :class => "error"}}.to_json
-      redirect '/auth/adduser?' + hash_to_query_string(params['user'])
-    end
-  end
-  # @todo Document this method
-  get '/users/:id/edit' do
-    login_required
-    redirect "/users" unless current_user.admin? || current_user.id.to_s == params[:id]
-    @user = TrstUser.find(params[:id])
-    haml :"/trst_auth/edit", :layout => false
-  end
-  # @todo Document this method
-  post '/users/:id/edit' do
-    login_required
-    redirect "/users" unless current_user.admin? || current_user.id.to_s == params[:id]
-    user = TrstUser.find(params[:id])
-    user_attributes = params[:user]
-    if params[:user][:password] == ""
-        user_attributes.delete("password")
-        user_attributes.delete("password_confirmation")
-    end
-    if user.update(user_attributes)
-      flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.edituser_msg'), :class => "info"}}.to_json
-      redirect '/'
-    else
-      flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.edituser_err') + @user.errors,:class => "error"}}.to_json
-      redirect "/users/#{user.id}/edit?" + hash_to_query_string(user_attributes)
-    end
-  end
-  # @todo Document this method
-  get '/users/:id/delete' do
-    login_required
-    redirect "/users" unless current_user.admin? || current_user.id.to_s == params[:id]
-    if TrstUser.delete(params[:id])
-      flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.deleteuser_msg'), :class => "info"}}.to_json
-    else
-      flash[:msg] = {:msg => {:txt => I18n.t('trst_auth.deleteuser_err'), :class => "error"}}.to_json
-    end
-    redirect '/'
   end
 
 end
