4523486b871277b2d3ffff907ea7fe18a85b7a3a
kfl62 <kfl62g@gmail.com>
2010-10-05 10:17:56 +0300

    cleanup and some docs

--- lib/models/trst_user.rb
+++ lib/models/trst_user.rb
@@ -38,50 +38,32 @@ class TrstUser
   attr_accessor :password, :password_confirmation
 
   class << self
-    # @todo Document this method
-    def get(hash)
-      if user = first(:conditions => hash)
-        user
-      else
-        nil
-      end
-    end
-    # @todo Document this method
-    def set(attributes)
-      user = new attributes
-      user.save
-      user
-    end
-    # @todo Document this method
-    def set!(attributes)
-      user = new attributes
-      user.save(:validate => false)
-      user
-    end
-    # @todo Document this method
-    def delete(pk)
-      user = find(pk)
-      user.destroy
-      user.destroyed?
-    end
-    # @todo Document this method
-   def authenticate(login_name, pass)
-      current_user = get(:login_name => login_name)
+    # Authentication based on name and password
+    # @param [String] login_name User name
+    # @param [String] pass User password
+    # @return [TrstUser] returns nil if no user with `login_name`, or password
+    # doesn't match
+    def authenticate(login_name, pass)
+      current_user = where(:login_name => login_name).first
       return nil if current_user.nil?
       return current_user if encrypt(pass, current_user.salt) == current_user.hashed_password
       nil
     end
-    # @todo Document this method
+    # Compare provided password with `hashed_password` using `Digest::SHA1.hexdigest`
+    # @param [String] pass User password
+    # @param [String] salt User salt
+    # @return [String] to compare with User's `hashed_password`
     def encrypt(pass, salt)
       Digest::SHA1.hexdigest(pass+salt)
     end
     # @todo Document this method
+    # @return [Array] 
     def user_related_to
       all.collect{|user| [user.id.to_s, user.login_name]}
     end
-
   end
-  # @todo Document this method
+  # Shortcut for `login_name`. Just for avoid errors like `TrstUser.first.name`
+  # @return [String] shortcut for `login_name`
   def name
     login_name
   end
@@ -100,26 +82,36 @@ class TrstUser
     self.id ==  -1 || (self.permission_grp & ["admin","web"]).length >= 1
   end
   # @todo Document this method
+  # @return [Array] 
   def tasks
     task_ids.values.flatten.uniq
   end
   # @todo Document this method
+  # @see #table_data 
+  # @return [Array] 
   def daily_tasks
     task_ids['daily_tasks']
   end
   # @todo Document this method
+  # @see #table_data 
+  # @return [Array] 
   def daily_tasks_name
     daily_tasks.collect{|id| TrstTask.find(id).name}
   end
   # @todo Document this method
+  # @see #table_data 
+  # @return [Array] 
   def other_tasks
     task_ids['other_tasks']
   end
   # @todo Document this method
+  # @see #table_data 
+  # @return [Array] 
   def other_tasks_name
     other_tasks.collect{|id| TrstTask.find(id).name}
   end
   # @todo Document this method
+  # @return [Array] 
   def table_data
     [{:css => "normal",:name => "login_name",:label => I18n.t("trst_user.login_name"),:value => login_name},
      {:css => "normal",:name => "email",:label => I18n.t("trst_user.email"),:value => email},
@@ -136,9 +128,9 @@ class TrstUser
   # @todo Document this method
   def random_string(len)
     chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
-    newpass = ""
-    1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
-    return newpass
+    newsalt = ""
+    1.upto(len) { |i| newsalt << chars[rand(chars.size-1)] }
+    return newsalt
   end
   # @todo Document this method
   def method_missing(m, *args)
