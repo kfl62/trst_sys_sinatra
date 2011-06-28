bded750a49f6ba273d687a7198f4e04a85172389
kfl62 <kfl62g@gmail.com>
2011-06-29 01:10:41 +0300

    unable to sort if field type is Array

--- lib/models/trst_partners_pf.rb
+++ lib/models/trst_partners_pf.rb
@@ -8,33 +8,41 @@ class TrstPartnersPf
   include Mongoid::Timestamps
 
   field :id_pn,               :type => String,        :default => "123456789ABCD"
-  field :name,                :type => Array,         :default => ["LastName","FirstName"]
+  field :name_last,           :type => String,        :default => "Last(Nume)"
+  field :name_first,          :type => String,        :default => "First(Prenume)"
   field :identities,          :type => Hash,          :default => {:id_sr => "KX", :id_nr => "123456",:id_by => "SPCLEP Cluj-Napoca", :id_on => "1900-01-01"}
-  field :address,             :type => Hash,          :default => {:city => "Cluj-Napoca", :street => "str.", :nr => "00", :bl => "__", :sc => "__", :et => "__", :ap => "__"}
+  field :address,             :type => Hash,          :default => {:city => "Cluj-Napoca", :street => "str.", :nr => "00", :bl => "-", :sc => "-", :et => "-", :ap => "-"}
   field :other,               :type => String,        :default => "Client"
 
   has_many :apps, :class_name => "TrstAccExpenditure", :inverse_of => :client
+
+  before_save :titleize_fields
   class << self
     # @todo
     def auto_search
       pfs = []
-      all.each do |pf|
-        label = "#{pf.id_pn} | #{pf.name_full}"
-        pfs << {:id => pf.id,:pn => pf.id_pn,:label => label}
+      all.asc(:name_last, :name_first).each do |pf|
+        if pf.id_pn == "123456789ABCD"
+          pf.delete
+        else
+          label = "#{pf.id_pn} | #{pf.name_full}"
+          pfs << {:id => pf.id,:pn => pf.id_pn,:label => label}
+        end
       end
       {:identifier => "id",:items => pfs}
     end
   end
   # @todo
   def name_full
-    name.join(' ')
+    [name_last,name_first].join(' ')
   end
+  alias :name :name_full
   # @todo
   def table_data
     [
       {:css => "normal",:name => "id_pn",:label => I18n.t("trst_partners_pf.id_pn"),:value => id_pn},
-      {:css => "normal",:name => "name,",:label => I18n.t("trst_partners_pf.name_last"),:value => name[0]},
-      {:css => "normal",:name => "name,",:label => I18n.t("trst_partners_pf.name_first"),:value => name[1]},
+      {:css => "normal",:name => "name_last",:label => I18n.t("trst_partners_pf.name_last"),:value => name_last},
+      {:css => "normal",:name => "name_first",:label => I18n.t("trst_partners_pf.name_first"),:value => name_first},
       {:css => "normal",:name => "identities,id_sr",:label => I18n.t("trst_partners_pf.identities.id_sr"),:value => identities["id_sr"]},
       {:css => "normal",:name => "identities,id_nr",:label => I18n.t("trst_partners_pf.identities.id_nr"),:value => identities["id_nr"]},
       {:css => "normal",:name => "identities,id_by",:label => I18n.t("trst_partners_pf.identities.id_by"),:value => identities["id_by"]},
@@ -56,5 +64,12 @@ class TrstPartnersPf
       city: address['city'], street: address['street'], nr: address["nr"], bl: address["bl"], sc: address["sc"], et: address["et"], ap: address["ap"],
       id_sr: identities["id_sr"], id_nr: identities["id_nr"], id_by: identities["id_by"], id_on: identities["id_on"]
     }
+  end
+  protected
+  # @todo
+  def titleize_fields
+    self.name_last = name_last.titleize
+    self.name_first = name_first.titleize
+    self.address[:street] = address[:street].titleize
   end
 end
