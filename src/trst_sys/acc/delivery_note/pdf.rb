54160ad28a23fb01e4f38770defac8f941d44ddb
kfl62 <kfl62g@gmail.com>
2011-07-17 23:04:31 +0300

    delivery_note, pdf generator

---
+++ src/trst_sys/acc/delivery_note/pdf.rb
@@ -0,0 +1,369 @@
+# st: font_face consolas; font_size 9
+# encoding: utf-8
+# Template for AdeverintaPredarePrimire.pdf
+require 'prawn/measurement_extensions'
+
+firm        = TrstFirm.first
+client      = TrstPartner.find(@object.client_id)
+transporter = TrstPartner.find(@object.transporter_id)
+
+pdf = Prawn::Document.new(
+  :page_size => 'A4',
+  :page_layout => :landscape,
+  :margin => [0.mm],
+  #:background => "public/images/pdf/ae_a3.jpg",
+  :info => {
+    :Title => "Formular de încărcare-descărcare deşeuri nepericuloase",
+    :Author => "kfl62",
+    :Subject => "Formular \"Încărcare-descărcare deşeuri nepericuloase\"",
+    :Keywords => "#{firm.name[1].titleize} Formular de încărcare-descărcare deşeuri nepericuloase",
+    :Creator => "http://#{firm.name[0].downcase}.trst.ro (using Sinatra, Prawn)",
+    :CreationDate => Time.now
+  }
+)
+
+pdf.font_families.update(
+  'Verdana' => {:bold => 'public/fonts/verdanab.ttf',
+                :italic => 'public/fonts/verdanai.ttf',
+                :bold_italic => 'public/fonts/verdanaz.ttf',
+                :normal => 'public/fonts/verdana.ttf'})
+pdf.font 'Verdana'
+
+# Ex. 1
+top = pdf.bounds.top - 10.mm
+left = 10.mm
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Formular de încărcare-descărcare deşeuri nepericuloase", :style => :bold, :size => 10, :align => :center, :valign => :center
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.indent 10.mm do
+    pdf.text "Serie şi număr: #{@object.name} din #{@object.id_date.to_s}", :style => :bold, :size => 8, :valign => :center
+  end
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 110.mm, :height => 14) do
+  pdf.text "Caracteristici deşeuri", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 80.mm, :height => 14) do
+  pdf.text "Denumire", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 80.mm,top], :width => 30.mm, :height => 14) do
+  pdf.text "Cod", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 110.mm,top + 14 ], :width => 20.mm, :height => 28) do
+  pdf.move_down 6
+  pdf.text "Cantitate", :style => :bold, :size => 8, :align => :center
+  pdf.text "-to-", :style => :bold, :size => 8, :align => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 80.mm, :height => 56) do
+  # Freight
+  pdf.move_down 4
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left,top], :width => 30.mm, :height => 56) do
+  # Cod
+  pdf.move_down 4
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 110.mm,top], :width => 20.mm, :height => 56) do
+  # Quantity
+  pdf.move_down 4
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Destinat", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 28) do
+  pdf.text "Colectării |_| Stocării temp. |_| Tratării |_| Valorificării |_| Eliminării |_|", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 28
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Documente însoţitoare", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 65.mm, :height => 14) do
+  pdf.text "Încărcare", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 65.mm,top], :width => 65.mm, :height => 14) do
+  pdf.text "Descărcare", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 65.mm, :height => 28) do
+  # id_main_doc
+  pdf.text "AE nr. #{@object.id_main_doc}/#{@object.id_date.to_s}", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 65.mm,top], :width => 65.mm, :height => 28) do
+  pdf.stroke_bounds
+  top -= 28
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Date identificare expeditor", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 56) do
+  pdf.bounding_box([5.mm, 50], :width => 25.mm) do
+    pdf.text "Firma:", :style => :bold, :size => 8
+    pdf.text "CCI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "CUI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Punct de lucru:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Aut. mediu:", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.bounding_box([30.mm, 50], :width => 105.mm) do
+    pdf.text firm.name[2], :style => :bold, :size => 8
+    pdf.text firm.identities['chambcom'], :style => :bold, :size => 8,:leading => 2
+    pdf.text firm.identities['fiscal'], :style => :bold, :size => 8,:leading => 2
+    pdf.text @object.unit.name[1], :style => :bold, :size => 8,:leading => 2
+    pdf.text "", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 42) do
+  pdf.text "Semnătură/Ştampilă", :style => :bold, :size => 8, :align => :right, :valign => :center
+  pdf.stroke_bounds
+  top -= 42
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Date identificare destinatar", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 56) do
+  pdf.bounding_box([5.mm, 50], :width => 25.mm) do
+    pdf.text "Firma:", :style => :bold, :size => 8
+    pdf.text "CCI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "CUI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Punct de lucru:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Aut. mediu:", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.bounding_box([30.mm, 50], :width => 105.mm) do
+    pdf.text firm.name[2], :style => :bold, :size => 8
+    pdf.text firm.identities['chambcom'], :style => :bold, :size => 8,:leading => 2
+    pdf.text firm.identities['fiscal'], :style => :bold, :size => 8,:leading => 2
+    pdf.text @object.unit.name[1], :style => :bold, :size => 8,:leading => 2
+    pdf.text "", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 42) do
+  pdf.text "Semnătură/Ştampilă", :style => :bold, :size => 8, :align => :right, :valign => :center
+  pdf.stroke_bounds
+  top -= 42
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Date identificare destinatar", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 56) do
+  pdf.bounding_box([5.mm, 50], :width => 25.mm) do
+    pdf.text "Firma:", :style => :bold, :size => 8
+    pdf.text "CCI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "CUI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Punct de lucru:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Aut. mediu:", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.bounding_box([30.mm, 50], :width => 105.mm) do
+    pdf.text firm.name[2], :style => :bold, :size => 8
+    pdf.text firm.identities['chambcom'], :style => :bold, :size => 8,:leading => 2
+    pdf.text firm.identities['fiscal'], :style => :bold, :size => 8,:leading => 2
+    pdf.text @object.unit.name[1], :style => :bold, :size => 8,:leading => 2
+    pdf.text "", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 42) do
+  pdf.text "Semnătură/Ştampilă", :style => :bold, :size => 8, :align => :right, :valign => :center
+  pdf.stroke_bounds
+end
+
+# Ex. 2
+top = pdf.bounds.top - 10.mm
+left = 160.mm
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Formular de încărcare-descărcare deşeuri nepericuloase", :style => :bold, :size => 10, :align => :center, :valign => :center
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.indent 10.mm do
+    pdf.text "Serie şi număr: #{@object.name} din #{@object.id_date.to_s}", :style => :bold, :size => 8, :valign => :center
+  end
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 110.mm, :height => 14) do
+  pdf.text "Caracteristici deşeuri", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 80.mm, :height => 14) do
+  pdf.text "Denumire", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 80.mm,top], :width => 30.mm, :height => 14) do
+  pdf.text "Cod", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 110.mm,top + 14 ], :width => 20.mm, :height => 28) do
+  pdf.move_down 6
+  pdf.text "Cantitate", :style => :bold, :size => 8, :align => :center
+  pdf.text "-to-", :style => :bold, :size => 8, :align => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 80.mm, :height => 56) do
+  # Freight
+  pdf.move_down 4
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left,top], :width => 30.mm, :height => 56) do
+  # Cod
+  pdf.move_down 4
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 110.mm,top], :width => 20.mm, :height => 56) do
+  # Quantity
+  pdf.move_down 4
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Destinat", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 28) do
+  pdf.text "Colectării |_| Stocării temp. |_| Tratării |_| Valorificării |_| Eliminării |_|", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 28
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Documente însoţitoare", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 65.mm, :height => 14) do
+  pdf.text "Încărcare", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 65.mm,top], :width => 65.mm, :height => 14) do
+  pdf.text "Descărcare", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 65.mm, :height => 28) do
+  # id_main_doc
+  pdf.text "AE nr. #{@object.id_main_doc}/#{@object.id_date.to_s}", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+end
+pdf.bounding_box([left + 65.mm,top], :width => 65.mm, :height => 28) do
+  pdf.stroke_bounds
+  top -= 28
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Date identificare expeditor", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 56) do
+  pdf.bounding_box([5.mm, 50], :width => 25.mm) do
+    pdf.text "Firma:", :style => :bold, :size => 8
+    pdf.text "CCI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "CUI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Punct de lucru:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Aut. mediu:", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.bounding_box([30.mm, 50], :width => 105.mm) do
+    pdf.text firm.name[2], :style => :bold, :size => 8
+    pdf.text firm.identities['chambcom'], :style => :bold, :size => 8,:leading => 2
+    pdf.text firm.identities['fiscal'], :style => :bold, :size => 8,:leading => 2
+    pdf.text @object.unit.name[1], :style => :bold, :size => 8,:leading => 2
+    pdf.text "", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 42) do
+  pdf.text "Semnătură/Ştampilă", :style => :bold, :size => 8, :align => :right, :valign => :center
+  pdf.stroke_bounds
+  top -= 42
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Date identificare destinatar", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 56) do
+  pdf.bounding_box([5.mm, 50], :width => 25.mm) do
+    pdf.text "Firma:", :style => :bold, :size => 8
+    pdf.text "CCI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "CUI:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Punct de lucru:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Aut. mediu:", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.bounding_box([30.mm, 50], :width => 105.mm) do
+    pdf.text firm.name[2], :style => :bold, :size => 8
+    pdf.text firm.identities['chambcom'], :style => :bold, :size => 8,:leading => 2
+    pdf.text firm.identities['fiscal'], :style => :bold, :size => 8,:leading => 2
+    pdf.text @object.unit.name[1], :style => :bold, :size => 8,:leading => 2
+    pdf.text "", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 42) do
+  pdf.text "Semnătură/Ştampilă", :style => :bold, :size => 8, :align => :right, :valign => :center
+  pdf.stroke_bounds
+  top -= 42
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 14) do
+  pdf.text "Date identificare transportator", :style => :bold, :size => 8, :align => :center, :valign => :center
+  pdf.stroke_bounds
+  top -= 14
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 56) do
+  pdf.bounding_box([5.mm, 50], :width => 25.mm) do
+    pdf.text "Firma:", :style => :bold, :size => 8
+    pdf.text "Delegat:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "CNP:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Auto Nr:", :style => :bold, :size => 8,:leading => 2
+    pdf.text "Licenţa de tran", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.bounding_box([30.mm, 50], :width => 105.mm) do
+    pdf.text transporter.name[2], :style => :bold, :size => 8
+    pdf.text @object.delegate_transp.name, :style => :bold, :size => 8,:leading => 2
+    pdf.text @object.delegate_transp.id_pn, :style => :bold, :size => 8,:leading => 2
+    pdf.text @object.id_platte, :style => :bold, :size => 8,:leading => 2
+    pdf.text "sport expiră la date de:", :style => :bold, :size => 8,:leading => 2
+  end
+  pdf.stroke_bounds
+  top -= 56
+end
+pdf.bounding_box([left,top], :width => 130.mm, :height => 42) do
+  pdf.text "Semnătură/Ştampilă", :style => :bold, :size => 8, :align => :right, :valign => :center
+  pdf.stroke_bounds
+end
+
+pdf.stroke_line(148.mm,5.mm,148.mm,10.mm)
+pdf.stroke_line(148.mm,200.mm,148.mm,205.mm)
+
+pdf.render()
