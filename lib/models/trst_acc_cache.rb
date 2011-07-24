e86920aab4c47d540ac589333e88d09459a5e8c9
kfl62 <kfl62g@gmail.com>
2011-07-24 15:28:04 +0300

    complete on TrstAccCache model and i18n

--- lib/models/trst_acc_cache.rb
+++ lib/models/trst_acc_cache.rb
@@ -7,7 +7,7 @@ class TrstAccCache
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :name
+  field :name,          :type => String,    :default => "DP_"
   field :id_date,       :type => Date,      :default => Date.new(Time.now.year,Time.now.month,Time.now.day)
   field :money_in,      :type => Float,     :default => 0.00
   field :money_out,     :type => Float,     :default => 0.00
@@ -15,6 +15,8 @@ class TrstAccCache
   field :expl,          :type => String,    :default => ""
 
   belongs_to :unit,       :class_name => "TrstFirmUnit",      :inverse_of => :dps
+
+  before_create :init_expl
 
   class << self
     # @todo
@@ -29,5 +31,22 @@ class TrstAccCache
     end
   end # Class methods
 
+  def table_data
+    [
+      {:css => "normal",:name => "name",:label => I18n.t("trst_acc_cache.name"),:value => name},
+      {:css => "date",:name => "id_date",:label => I18n.t("trst_acc_cache.id_date"),:value => id_date},
+      {:css => "integer",:name => "money_in",:label => I18n.t("trst_acc_cache.money_in"),:value => "%.2f" % money_in},
+      {:css => "integer",:name => "money_out",:label => I18n.t("trst_acc_cache.money_out"),:value => "%.2f" % money_out},
+      {:css => "integer",:name => "money_stock",:label => I18n.t("trst_acc_cache.money_stock"),:value => "%.2f" % money_stock},
+      {:css => "normal",:name => "expl",:label => I18n.t("trst_acc_cache.expl"),:value => expl}
+    ]
+  end
+
+  protected
+
+  # @todo
+  def init_expl
+    self.expl = I18n.t('trst_acc_cache.expl_value')
+  end
 
 end
