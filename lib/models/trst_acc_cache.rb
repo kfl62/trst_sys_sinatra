33409fc4033835980581b0d006f030afa6dc570a
kfl62 <kfl62g@gmail.com>
2011-07-27 22:21:25 +0300

    TrstAccCache query and view, just for demo :)

--- lib/models/trst_acc_cache.rb
+++ lib/models/trst_acc_cache.rb
@@ -21,13 +21,28 @@ class TrstAccCache
   class << self
     # @todo
     def daily(day)
-      crit = where(:id_date => DateTime.strptime("#{day}","%F").to_time)
-      [crit.sum(:money_in),crit.sum(:money_out),crit.sum(:money_stock)]
+      where(:id_date => DateTime.strptime("#{day}","%F").to_time)
     end
     # @todo
     def pos(slg)
       slg = slg.upcase
       where(:unit_id => TrstFirm.unit_id_by_unit_slug(slg))
+    end
+    # @todo
+    def query(m = nil)
+      m = m || Date.today.month
+      unit_id = first.unit_id
+      retval = []; tot_in = 0; tot_out = 0
+      (1..31).each do |i|
+        day = "2011-#{"%02i" % m}-#{"%02i" % i}"
+        sum_in  = daily(day).sum(:money_in) || 0.0
+        tot_in += sum_in
+        sum_out = TrstAccExpenditure.where(:unit_id => unit_id).daily(day).sum(:sum_out) || 0.0
+        tot_out += sum_out
+        retval << [day, sum_in.round(2), sum_out.round(2), (tot_in - tot_out).round(2)] unless (sum_in == 0 && sum_out ==0)
+      end
+      retval.push(["Total", tot_in.round(2), tot_out.round(2), (tot_in - tot_out).round(2)])
+      retval
     end
   end # Class methods
 
