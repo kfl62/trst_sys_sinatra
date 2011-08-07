0319e053dbf4ad0f14545c4b1a0e442818ed7b95
kfl62 <kfl62g@gmail.com>
2011-08-07 19:24:50 +0300

    TrstAccGrn model and CRUD template

---
+++ lib/models/trst_acc_grn.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+=begin
+#Goods received note (Accounting)
+=end
+
+class TrstAccGrn
+  include Mongoid::Document
+  include Mongoid::Timestamps
+end
---
+++ src/trst_sys/acc/grn/pdf.rb
@@ -0,0 +1,4 @@
+# st: font_face consolas; font_size 9
+# encoding: utf-8
+# Template for Notă De Recepţie
+require 'prawn/measurement_extensions'
