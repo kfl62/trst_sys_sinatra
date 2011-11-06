eb80d7482a55f46a476a894a3f749c1723263b23
kfl62 <kfl62g@gmail.com>
2011-11-06 09:25:10 +0200

    forgotten mode lines

--- src/trst_sys/acc/grn/pdf.rb
+++ src/trst_sys/acc/grn/pdf.rb
@@ -1,4 +1,3 @@
-# st: font_face consolas; font_size 9
 # encoding: utf-8
 # Template for Notă De Recepţie
 require 'prawn/measurement_extensions'
--- src/trst_sys/acc/invoice/pdf.rb
+++ src/trst_sys/acc/invoice/pdf.rb
@@ -1,4 +1,3 @@
-# st: font_face consolas; font_size 9
 # encoding: utf-8
 # Template for Invoice
 require 'prawn/measurement_extensions'
