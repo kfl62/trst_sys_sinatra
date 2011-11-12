07bfa7c65505cfef69c61a06e3f6b4e8a8ccb5e1
kfl62 <kfl62g@gmail.com>
2011-11-12 21:46:11 +0200

    TrstAccInvoice model and i18n

--- lib/models/trst_acc_invoice.rb
+++ lib/models/trst_acc_invoice.rb
@@ -6,4 +6,68 @@
 class TrstAccInvoice
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  field :name,              :type => String
+  field :id_date,           :type => Date
+  field :id_main_doc,       :type => String
+  field :id_delegate_c,     :type => String
+  field :sum,               :type => Float,       :default => 0.00
+  field :payed,             :type => Boolean,     :default => false
+  field :payment_doc,       :type => String
+  field :payment_deadline,  :type => Date
+  field :payment_date,      :type => Array,       :default => []
+
+  belongs_to  :client,        :class_name => "TrstPartner",         :inverse_of => :invoices
+  has_many    :delivery_notes,:class_name => "TrstAccDeliveryNote", :inverse_of => :invoice
+  embeds_many :freights,      :class_name => "TrstAccInvoiceFreight"
+
+  before_create :increment_name_date
+  after_destroy :restore_delivery_notes
+
+  # @todo
+  def delegate
+    TrstPartner.find(client_id).delegates.find(id_delegate_c) rescue TrstPartner.find(client_id).delegates.first
+  end
+  # @todo
+  def update_delivery_notes(add = true)
+    self.delivery_notes.each do |dn|
+      dn.charged = add
+      dn.save
+    end
+    self.freights.each do |f|
+      self.delivery_notes.each do |dn|
+        dn.freights.each do |df|
+          if add
+            df.pu_invoice = f.pu if df.freight.id_stats == f.id_stats
+          else
+            df.pu_invoice = 0.00 if df.freight.id_stats == f.id_stats
+          end
+          df.save
+        end
+      end
+    end
+  end
+  # @todo
+  def file_name
+    "NIR_INV_#{id_main_doc}"
+  end
+  # @todo
+  def pdf_template
+    'pdf'
+  end
+
+  protected
+  # @todo
+  def increment_name_date
+    if TrstAccInvoice.last.freights.empty?
+      TrstAccInvoice.last.destroy
+    end unless TrstAccInvoice.last.nil?
+    self.name = TrstAccInvoice.asc(:name).last.name.next rescue "#{TrstFirm.first.name[0][0..2].upcase}_INV_001"
+    self.id_main_doc = TrstAccInvoice.asc(:name).last.id_main_doc.next rescue "#{TrstFirm.first.name[0][0..2].upcase}-"
+    self.id_date = Date.today
+  end
+  # @todo
+  def restore_delivery_notes
+    self.update_delivery_notes(false)
+  end
 end
