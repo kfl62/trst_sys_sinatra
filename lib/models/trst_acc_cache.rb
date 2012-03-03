64a46342462bf1233e27fb7b6071b034db572485
kfl62 <kfl62g@gmail.com>
2012-03-03 10:43:51 +0200

    TrstAccCache model cleanup

--- lib/models/trst_acc_cache.rb
+++ lib/models/trst_acc_cache.rb
@@ -8,7 +8,7 @@ class TrstAccCache
   include Mongoid::Timestamps
 
   field :name,          :type => String
-  field :id_date,       :type => Date,      :default => Date.new(Time.now.year,Time.now.month,Time.now.day)
+  field :id_date,       :type => Date
   field :id_intern,     :type => Boolean,   :default => false
   field :money_in,      :type => Float,     :default => 0.00
   field :money_out,     :type => Float,     :default => 0.00
@@ -26,7 +26,7 @@ class TrstAccCache
       y ||= Date.today.year
       m ||= Date.today.month
       d ||= Date.today.mday
-      where(:id_date => Time.utc(y,m,d)).asc(:name)
+      where(:id_date => Time.utc(y,m,d))
     end
     # @todo
     def monthly(y = nil, m = nil)
@@ -38,12 +38,11 @@ class TrstAccCache
     end
     # @todo
     def pos(slg)
-      slg = slg.upcase
-      where(:unit_id => TrstFirm.unit_id_by_unit_slug(slg)).asc(:id_date)
+      where(:unit_id => TrstFirm.pos(slg))
     end
     # @todo
     def by_unit_id(u)
-      where(:unit_id => u).asc(:id_date)
+      where(:unit_id => u)
     end
     # @todo
     def query(y = nil, m = nil)
@@ -51,35 +50,35 @@ class TrstAccCache
       y ||= Date.today.year
       m ||= Date.today.month
       days_in_month = (Date.new(y, 12, 31) << (12-m)).day
-      unit_id = first.unit_id
-      retval, tot_out = [], 0
-      tot_in = monthly(y,m).sum(:money_stock) || 0
-      tot_tot = 0
-      (1..days_in_month).each do |i|
+      stk_in = monthly(y,m).sum(:money_stock) || 0
+      tot_in, tot_out, tot_tot = 0, 0, 0
+      retval = (1..days_in_month).each_with_object([]) do |i,a|
         day = Date.new(y,m,i).to_s
         sum_in  = daily(day).sum(:money_in) || 0.0
         tot_in += sum_in
-        sum_out = TrstAccExpenditure.by_unit_id(unit_id).daily(day).sum(:sum_out) || 0.0
+        sum_out = first.unit.apps.daily(day).sum(:sum_out) || 0.0
         tot_out += sum_out
-        sum_tot = TrstAccExpenditure.by_unit_id(unit_id).daily(day).sum(:sum_100) || 0.0
+        sum_tot = first.unit.apps.daily(day).sum(:sum_100) || 0.0
         tot_tot += sum_tot
-        retval << [day, sum_in.round(2), sum_out.round(2), (tot_in - tot_out).round(2), sum_tot.round(2)] unless (sum_in == 0 && sum_out ==0)
+        a << [day, sum_in.round(2), sum_out.round(2), (tot_in + stk_in - tot_out).round(2), sum_tot.round(2)] unless (sum_in == 0 && sum_out ==0)
       end
-      retval.push(["Total", tot_in.round(2), tot_out.round(2), (tot_in - tot_out).round(2),tot_tot.round(2)])
-      retval
+      retval.push(["Total", tot_in.round(2), tot_out.round(2), (tot_in + stk_in - tot_out).round(2),tot_tot.round(2)])
     end
     # @todo
     def balance(y = nil, m = nil)
       y ||= Date.today.year
       m ||= Date.today.month
-      unit_id = first.unit_id
       stk = monthly(y,m).sum(:money_stock) || 0
       ins = monthly(y,m).sum(:money_in) || 0
-      out = TrstAccExpenditure.by_unit_id(unit_id).monthly(y,m).sum(:sum_out) || 0
+      out = first.unit.apps.monthly(y,m).sum(:sum_out) || 0
       [stk, ins, out, stk + ins - out]
     end
   end # Class methods
-
+  # @todo
+  def unit
+    TrstFirm.unit_by_unit_id(self.unit_id)
+  end
+  # @todo
   def table_data
     [
       {:css => "admin",:name => "name",:label => I18n.t("trst_acc_cache.name"),:value => name},
@@ -90,9 +89,7 @@ class TrstAccCache
       {:css => "normal",:name => "expl",:label => I18n.t("trst_acc_cache.expl"),:value => expl}
     ]
   end
-
   protected
-
   # @todo
   def init_name_date_expl
     self.name = "DP_NR-#{Date.today.to_s}"
