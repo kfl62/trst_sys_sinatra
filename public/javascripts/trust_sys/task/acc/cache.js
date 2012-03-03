9507959dd336966068aff14dd587dad680431e6b
kfl62 <kfl62g@gmail.com>
2012-03-03 17:09:57 +0200

    new cache.js

--- public/javascripts/trust_sys/task/acc/cache.js
+++ public/javascripts/trust_sys/task/acc/cache.js
@@ -1,6 +1,33 @@
 dojo.mixin(trst.task.acc,{
   cache: {
     init: function(){
+      // Connect select, 'Post' on filter
+      if (trst.task.acc.hd.action == 'filter'){
+        dojo.query('select.dp').onchange(function(e){
+          if (selectedValue(e.target) != 'null'){
+            trst.task.init(trst.task.acc.hd.task_id,'get',selectedValue(e.target))
+          }
+        })
+        dojo.query('.button.post.mc-evt').onclick(function(){
+           trst.task.init(trst.task.acc.hd.task_id,'post','new')
+        })
+      }
+      // Connect buttons ('Save','Cancel','Delete', etc.) except 'Post'
+      this.connectButtons()
+    },
+    connectButtons: function(){
+      dojo.query('.button.save.mc-evt').onclick(function(){
+        trst.task.init(trst.task.acc.hd.task_id,'put',trst.task.acc.hd.object_id)
+      })
+      dojo.query('.button.put.mc-evt').onclick(function(){
+        trst.task.init(trst.task.acc.hd.task_id,'put',trst.task.acc.hd.object_id)
+      })
+      dojo.query('.button.cancel.mc-evt').onclick(function(){
+        trst.task.destroy()
+      })
+      dojo.query('.button.delete.mc-evt').onclick(function(){
+        trst.task.init(trst.task.acc.hd.task_id,'delete',trst.task.acc.hd.object_id)
+      })
     }
   }
 })
