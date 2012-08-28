dd4238a374a19dd986f9aa06bad6da30cf065d7c
kfl62 <kfl62g@gmail.com>
2012-08-28 23:07:31 +0300

    lib: Trst::DateHelpers

---
+++ lib/trst_date_helpers.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+module Trst
+  module DateHelpers
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      # @todo
+      def yearly(y = nil)
+        y = y.to_i  if y.is_a? String
+        y ||= Date.today.year
+        where(:id_date.gte => DateTime.new(y).to_time, :id_date.lt => DateTime.new(y + 1).to_time)
+      end
+      # @todo
+      def monthly(y = nil, m = nil)
+        y,m = y.split('-').map{|s| s.to_i} if y.is_a? String
+        y ||= Date.today.year
+        m ||= Date.today.month
+        mb = DateTime.new(y, m)
+        me = m.to_i == 12 ? DateTime.new(y + 1, 1) : DateTime.new(y, m + 1)
+        where(:id_date.gte => mb.to_time, :id_date.lt => me.to_time)
+      end
+      # @todo
+      def daily(y = nil, m = nil, d = nil)
+        y,m,d = y.split('-').map{|s| s.to_i} if y.is_a? String
+        y ||= Date.today.year
+        m ||= Date.today.month
+        d ||= Date.today.mday
+        where(:id_date => Time.utc(y,m,d))
+      end
+    end #Class methods
+
+  end
+end
