7261fc83ab7b2ad14f9b86817691414fe1a51eba
kfl62 <kfl62g@gmail.com>
2012-09-13 06:53:45 +0300

    helpers: date_helpers, only numeric arguments

--- lib/trst_date_helpers.rb
+++ lib/trst_date_helpers.rb
@@ -6,22 +6,19 @@ module Trst
     module ClassMethods
       # @todo
       def yearly(y = nil)
-        y = y.to_i  if y.is_a? String
         y ||= Date.today.year
         where(:id_date.gte => DateTime.new(y).to_time, :id_date.lt => DateTime.new(y + 1).to_time)
       end
       # @todo
       def monthly(y = nil, m = nil)
-        y,m = y.split('-').map{|s| s.to_i} if y.is_a? String
         y ||= Date.today.year
         m ||= Date.today.month
         mb = DateTime.new(y, m)
-        me = m.to_i == 12 ? DateTime.new(y + 1, 1) : DateTime.new(y, m + 1)
+        me = m == 12 ? DateTime.new(y + 1, 1) : DateTime.new(y, m.to_i + 1)
         where(:id_date.gte => mb.to_time, :id_date.lt => me.to_time)
       end
       # @todo
       def weekly(y = nil, w = nil)
-        y,w = y.split('-').map{|s| s.to_i} if y.is_a? String
         y ||= Date.today.year
         w ||= Date.today.cweek
         wb = Date.commercial(y, w, 1)
@@ -30,7 +27,6 @@ module Trst
       end
       # @todo
       def daily(y = nil, m = nil, d = nil)
-        y,m,d = y.split('-').map{|s| s.to_i} if y.is_a? String
         y ||= Date.today.year
         m ||= Date.today.month
         d ||= Date.today.mday
