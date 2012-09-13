38e98aad1956f5aa5898ec882da1e08776428b53
kfl62 <kfl62g@gmail.com>
2012-09-13 07:03:03 +0300

    models: Trst::Freight, extract common logic from modules

--- system/models/trst/freight.rb
+++ system/models/trst/freight.rb
@@ -11,7 +11,23 @@ module Trst
     field :id_stats,    type: String
 
     class << self
+      # @todo
+      def ids
+        all.map(&:id)
+      end
+      # @todo
+      def keys(pu = true)
+        ks = all.each_with_object([]){|f,k| k << "#{f.id_stats}"}.uniq.sort!
+        ks = all.each_with_object([]){|f,k| k << "#{f.id_stats}_#{"%05.2f" % f.pu}"}.uniq.sort! if pu
+        ks
+      end
+      # @todo
+      def stats_pos(*args)
+        opts = args.last.is_a?(Hash) ? {}.merge!(args.pop) : {}
+        asc(:id_stats).each_with_object([]) do |f,a|
+          a << [f.id,f.name,*f.stats_sum(*args,opts)]
+        end
+      end
     end # Class methods
-
   end # Freight
 end # Trst
