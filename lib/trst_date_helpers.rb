ebb92207662515752cef9becb018cf37b25c64aa
kfl62 <kfl62g@gmail.com>
2012-09-15 18:04:48 +0300

    helper: date_helpers #period

--- lib/trst_date_helpers.rb
+++ lib/trst_date_helpers.rb
@@ -10,23 +10,33 @@ module Trst
         where(:id_date.gte => DateTime.new(y).to_time, :id_date.lt => DateTime.new(y + 1).to_time)
       end
       # @todo
-      def monthly(y = nil, m = nil)
+      def monthly(y = nil,m = nil)
         y ||= Date.today.year
         m ||= Date.today.month
         mb = DateTime.new(y, m)
-        me = m == 12 ? DateTime.new(y + 1, 1) : DateTime.new(y, m.to_i + 1)
+        me = m == 12 ? DateTime.new(y + 1, 1) : DateTime.new(y, m + 1)
         where(:id_date.gte => mb.to_time, :id_date.lt => me.to_time)
       end
       # @todo
-      def weekly(y = nil, w = nil)
+      def period(y = nil,m = nil,d = nil,p = nil)
+        y ||= Date.today.year
+        m ||= Date.today.month
+        d ||= Date.today.mday
+        p ||= 1
+        pb  = Time.utc(y,m,d)
+        pe  = pb + p.day
+        where(:id_date.gte => pb, :id_date.lt => pe)
+      end
+       # @todo
+      def weekly(y = nil,w = nil)
         y ||= Date.today.year
         w ||= Date.today.cweek
         wb = Date.commercial(y, w, 1)
         we = Date.commercial(y, w, 7)
         where(:id_date.gte => wb.to_time, :id_date.lte => we.to_time)
       end
-      # @todo
-      def daily(y = nil, m = nil, d = nil)
+     # @todo
+      def daily(y = nil,m = nil,d = nil)
         y ||= Date.today.year
         m ||= Date.today.month
         d ||= Date.today.mday
