cb617ea8b1a815294ecc6591f4f8976818092aa8
kfl62 <kfl62g@gmail.com>
2012-09-15 18:04:16 +0300

    I18n fall backs, for mongoid inspect

--- config/boot.rb
+++ config/boot.rb
@@ -17,8 +17,11 @@ Dir['system/*'].each do |dir|
   $LOAD_PATH.unshift(dir) unless $LOAD_PATH.include?(dir)
 end
 # Add path for translations and default language
+require "i18n/backend/fallbacks"
+I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 I18n.load_path += Dir.glob(File.join(TRST_ROOT, 'system','i18n','**','*','*.yml'))
 I18n.default_locale = :ro
+I18n.fallbacks[:ro]=[:ro,:en]
 # Sintra default settings
 Sinatra::Base.set(:root, TRST_ROOT)
 Sinatra::Base.set(:cdn,  true)
