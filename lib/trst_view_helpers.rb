8c03ce9dc3a9c2b8894ced01009a617abac3606d
kfl62 <kfl62g@gmail.com>
2012-09-26 14:01:23 +0300

    helpers: extend String #to_bool

--- lib/trst_view_helpers.rb
+++ lib/trst_view_helpers.rb
@@ -39,3 +39,11 @@ module Trst
     end
   end
 end
+
+class String
+  def to_bool
+    return true if self == true || self =~ (/(true|t|yes|y|1)$/i)
+    return false if self == false || self.empty? || self =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+  end
+end
