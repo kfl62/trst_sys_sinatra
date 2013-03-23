88fe4c1b6357da64c73d1e84540c55151177867a
kfl62 <kfl62g@gmail.com>
2013-03-23 10:40:57 +0200

    assets: upgrade jquery-ui 1.10.2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 require.config({
   paths: {
     'jquery': 'http://code.jquery.com/jquery-1.9.1.min',
-    'jquery-ui': 'http://code.jquery.com/ui/1.9.2/jquery-ui.min',
+    'jquery-ui': 'http://code.jquery.com/ui/1.10.2/jquery-ui.min',
     'async': 'plugins/async'
   },
   priority: ['jquery']
