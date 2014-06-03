7e1e3d65518b7660939ef72fafe214ed55769e81
kfl62 <kfl62g@gmail.com>
2014-06-03 14:48:30 +0300

    modules: `Trst::Bank` skeleton, use as embedded

---
+++ system/models/trst/bank.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+module Trst
+  class Bank
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    include Trst::ViewHelpers
+
+    field :name,      type: String
+    field :swift,     type: String
+
+    embedded_in :firm,      class_name: 'Trst::PartnerFirm',  inverse_of: :banks
+
+  end # Bank
+end # Trst
