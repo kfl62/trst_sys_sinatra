86836f8d78fc3b1323b0d5aa30a0028e52f19adc
kfl62 <kfl62g@gmail.com>
2016-08-28 15:10:12 +0300

    models: `Trst::Person` remove unneeded defaults

--- system/models/trst/person.rb
+++ system/models/trst/person.rb
@@ -5,14 +5,14 @@ module Trst
     include Mongoid::Timestamps
     include ViewHelpers
 
-    field :id_pn,             type: String,                             default: '123456789012'
-    field :name_last,         type: String,                             default: 'LastName'
-    field :name_frst,         type: String,                             default: 'FirstName'
-    field :id_doc,            type: Hash,                               default: {"type" => 'CI', "sr" => 'KX', "nr" => '123456', "by" => 'SPCLEP Cluj-Napoca', "on" => '1980-01-01'}
-    field :email,             type: String,                             default: '-'
-    field :phone,             type: String,                             default: '-'
-    field :mobile,            type: String,                             default: '-'
-    field :other,             type: String,                             default: 'Client'
+    field :id_pn              ,type: String
+    field :name_last          ,type: String
+    field :name_frst          ,type: String
+    field :id_doc             ,type: Hash         ,default: {"type" => 'CI', "by" => 'SPCLEP'}
+    field :email              ,type: String
+    field :phone              ,type: String
+    field :mobile             ,type: String
+    field :other              ,type: String
 
     validates_presence_of   :name_last, :name_frst
     validates_uniqueness_of :id_pn, :unless => Proc.new{|p| p.id_pn == '-'}
@@ -53,10 +53,10 @@ module Trst
     protected
     # @todo
     def beautify
-      self.name_frst = name_frst.titleize if name_frst
-      self.name_last = name_last.titleize if name_last
-      self.id_doc['type'] = id_doc['type'].upcase if id_doc['type']
-      self.id_doc['sr']   = id_doc['sr'].upcase if id_doc['sr']
+      name_frst = name_frst.titleize if name_frst
+      name_last = name_last.titleize if name_last
+      id_doc['type'] = id_doc['type'].upcase if id_doc['type']
+      id_doc['sr']   = id_doc['sr'].upcase if id_doc['sr']
     end
   end # Person
 
